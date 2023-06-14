902ca95a08cd28bbe98ca7455ec3366c6b3938ec
Shaun Savage <savages@savages.com>
2023-06-14 22:10:40 +0000

    added wworker and mover about to a component

---
+++ src/components/About.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+    <div>
+        <button @click="clk($event)">Click</button>
+    </div>
+</template>
+  
+<script setup lang="ts">
+
+import init, {greet} from '../../wworker/pkg'
+const wasm = await init()
+
+console.log("ABOUT:SETUP",wasm)
+
+
+
+function clk(evt: Event) {
+    console.log("Click", evt)
+    greet()
+}
+</script>
+
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 2vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+</style>
+  
